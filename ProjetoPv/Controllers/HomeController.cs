21f81b41ee8defff9b497bcb67b015ad4fdbe9e2
Pedro <65790728+Pedro@users.noreply.github.com>
2023-07-11 09:33:33 +0100

    fase 5

--- ProjetoPv/Controllers/HomeController.cs
+++ ProjetoPv/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
-﻿    using Microsoft.AspNetCore.Authorization;
+﻿using Humanizer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ProjetoPv.Areas.Identity.Data;
 using ProjetoPv.Models;
+using System.Data;
 using System.Diagnostics;
 using static System.Formats.Asn1.AsnWriter;
 
@@ -16,8 +18,7 @@ namespace ProjetoPv.Controllers
         private readonly ILogger<HomeController> _logger;
         private UserManager<ProjetoPvUser> _userManager;
         private RoleManager<IdentityRole> _roleManager;
-
-
+        
 
         public HomeController( RoleManager<IdentityRole> roleManager,UserManager<ProjetoPvUser> userManager, ILogger<HomeController> logger)
         {
@@ -26,11 +27,16 @@ namespace ProjetoPv.Controllers
             _roleManager = roleManager;
         }
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            var users = _userManager.Users.ToList();
+            var users = await _userManager.Users
+                .Select(user => new {
+                    Id = user.Id,
+                    Username = user.UserName,
+                    Role = _userManager.GetRolesAsync(user).Result.FirstOrDefault()
+                }).ToListAsync();
+
             return View(users);
-        
         }
 
         public IActionResult Privacy()
@@ -38,15 +44,60 @@ namespace ProjetoPv.Controllers
             return View();
         }
 
-        public async Task<IActionResult> Edit(int? id)
+        public async Task<IActionResult> Edit(string id)
         {
-            var roles = _roleManager.Roles.ToList();
-            return View(roles);
-            //ViewData["Id"] = new SelectList(_userManager.Users,"Id");
-            //return View(_userManager.Users.ToList());
+            // Get the user
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            // Get the list of user roles
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            // Get the list of all roles
+            var allRoles = await _roleManager.Roles.ToListAsync();
+
+            // Build the view model
+            var model = new ChangeRoleViewModel
+            {
+                UserId = user.Id,
+                UserEmail = user.Email,
+                UserRole = userRoles.FirstOrDefault(),
+                AllRoles = allRoles
+            };
+
+            // Send the model to the view
+            return View(model);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, ChangeRoleViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user != null)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var currentRole = userRoles.FirstOrDefault();
+
+                if (currentRole != null && !currentRole.Equals(model.UserRole))
+                {
+                    await _userManager.RemoveFromRoleAsync(user, currentRole);
+                }
+                if (await _roleManager.RoleExistsAsync(model.UserRole))
+                {
+                    await _userManager.AddToRoleAsync(user, model.UserRole);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+    
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
---
+++ ProjetoPv/Models/ChangeRoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ProjetoPv.Models
+{
+    public class ChangeRoleViewModel
+    {
+
+        public string UserId { get; set; }
+        public string UserEmail { get; set; }
+        public string UserRole { get; set; }
+        public IEnumerable<IdentityRole> AllRoles { get; set; }
+    }
+}
+
